b14aa4f31795c7fe8e2308d62b1ea8cbc68485ea
mjwachira <mjwachira1@gmail.com>
2024-06-17 17:54:19 +0300

    use state

--- src/Components/state/User.tsx
+++ src/Components/state/User.tsx
@@ -5,43 +5,45 @@ type AuthUser ={
     email:string
 }
 
-export const User =()=>{
-    const [user, setUser] = useState<AuthUser>({}as AuthUser)
-    const handleLogin = () =>{
-        setUser({
-            name:'Johnny',
-            email:'johnny@gmail.com'
-        })
-    }
-  
-    return(
-        <div>
-            <button onClick={handleLogin} >Login</button>
-           
-            <div>User name is {user?.name} </div>
-            <div>User email is {user?.email} </div>
-        </div>
-    )
-}
-
-
 // export const User =()=>{
-//     const [user, setUser] = useState<AuthUser | null>(null)
+//     const [user, setUser] = useState<AuthUser>({}as AuthUser)
 //     const handleLogin = () =>{
 //         setUser({
 //             name:'Johnny',
 //             email:'johnny@gmail.com'
 //         })
 //     }
-//     const handleLogout = () =>{
-//         setUser(null)
-//     }
+  
 //     return(
 //         <div>
 //             <button onClick={handleLogin} >Login</button>
-//             <button onClick={handleLogout}>Logout</button> 
+           
 //             <div>User name is {user?.name} </div>
 //             <div>User email is {user?.email} </div>
 //         </div>
 //     )
 // }
+
+
+
+
+export const User =()=>{
+    const [user, setUser] = useState<AuthUser | null>(null)
+    const handleLogin = () =>{
+        setUser({
+            name:'Johnny',
+            email:'johnny@gmail.com'
+        })
+    }
+    const handleLogout = () =>{
+        setUser(null)
+    }
+    return(
+        <div>
+            <button onClick={handleLogin} >Login</button>
+            <button onClick={handleLogout}>Logout</button> 
+            <div>User name is {user?.name} </div>
+            <div>User email is {user?.email} </div>
+        </div>
+    )
+}
