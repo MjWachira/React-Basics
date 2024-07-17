7340791fdb46aac82b4253924bd6c1b52b73436f
mjwachira <mjwachira1@gmail.com>
2024-07-17 17:21:54 +0300

    Buttons

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -4,5 +4,7 @@ type ButtonProps ={
 
 
 export const Button =(props:ButtonProps) =>{
-    return <button onClick={(event)=>props.handleClick(event,1)}>Click</button>
+    return <button onClick={(event)=>props.handleClick(event,1)}>
+        Click
+        </button>
 }
